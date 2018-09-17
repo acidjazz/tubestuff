3470c883330458b00543058f8835c76663d50535
kevin olson <acidjazz@gmail.com>
2018-09-17 15:36:45 -0500

    🎉 Initial commit.

---
+++ readme.md
@@ -0,0 +1,6 @@
+
+> YouTube URL and API helpers
+
+[![Packagist License](https://poser.pugx.org/acidjazz/tubethings/license.png)](https://choosealicense.com/licenses/apache-2.0/)
+[![Latest Stable Version](https://poser.pugx.org/acidjazz/tubethings/version.png)](https://packagist.org/packages/acidjazz/tubethings)
+[![Total Downloads](https://poser.pugx.org/acidjazz/tubethings/d/total.png)](https://packagist.org/packages/barryvdh/tubethings)
---
+++ src/TubeThings.php
@@ -0,0 +1,177 @@
+<?php
+
+namespace acidjazz\TubeThings;
+/**
+ * YouTube URL and API support
+ *
+ * @package TubeThings
+ * @author kevin olson <acidjazz@gmail.com>
+ * @version 0.1
+ * @copyright (C) 2018 kevin olson <acidjazz@gmail.com>
+ * @license APACHE
+ */
+
+use Google_Client;
+use Google_Service_YouTube;
+use Goutte\Client;
+use App\Models\Video;
+
+ class TubeThings {
+
+   /**
+    * Google Client
+    */
+   private $client;
+
+   /**
+    * Google_Services_YouTube
+    */
+   private $youtube;
+
+   public function __construct()
+   {
+      $this->client = new Google_Client();
+      $this->client->setApplicationName("TubeThings");
+      $this->client->setDeveloperKey(config('services.google.api_key'));
+      $this->youtube = new Google_Service_YouTube($this->client);
+   }
+
+    /**
+     * parse different urls and determine types and ids
+     *
+     * @param String $url
+     * @return Array
+     */
+    public function parse($url)
+    {
+
+        $type = 'unknown';
+        $id = false;
+
+        $parsed = parse_url($url);
+
+        // https://www.youtube.com/channel/UC6MFZAOHXlKK1FI7V0XQVeA
+        if (strpos($parsed['path'], '/channel') === 0)
+        {
+            $type = 'channel';
+            $id = explode('/', $parsed['path'])[2];
+        }
+
+        // https://www.youtube.com/user/ProZD
+        if (strpos($parsed['path'], '/user') === 0)
+        {
+            $type = 'channel';
+            $id = $this->getChannelId(explode('/', $parsed['path'])[2]);
+        }
+
+        // https://www.youtube.com/watch?v=4ZK8Z8hulFg
+        if (strpos($parsed['path'], '/watch') === 0)
+        {
+            $type = 'video';
+            parse_str($parsed['query'], $query);
+            $id = $query['v'];
+        }
+
+        // https://youtu.be/aJX4ytfqw6k
+        if ($parsed['host'] ===  'youtu.be')
+        {
+            $type = 'video';
+            $id = substr($parsed['path'], 1);
+        }
+
+        return ['type' => $type, 'id' => $id];
+    }
+    /**
+     * Get a channel ID from a user
+     *
+     * @param String $user
+     * @return String
+     */
+    private function getChannelId($user)
+    {
+        $client = new Client();
+        $crawler = $client->request('GET', 'https://www.youtube.com/user/'.$user);
+        $url = $crawler->filter('link[rel="canonical"]')->attr('href');
+        return explode('/', $url)[4];
+    }
+
+    /**
+     * Get list of videos from a channel ID
+     *
+     * @param String $id
+     * @param String $pageToken
+     * @param App\Models\User $user
+     * @return Object
+     */
+    public function getChannel($id,$pageToken=null,$user=false)
+    {
+
+      $list = $this->youtube->search->listSearch(
+        ['snippet'],
+        ['channelId' => $id, 'maxResults' => 9,'pageToken' => $pageToken, 'order' => 'date']
+      );
+
+      $results = [];
+      $results['channelId'] = $list->items[0]->snippet->channelId;
+      $results['channelTitle'] = $list->items[0]->snippet->channelTitle;
+      $results['nextPageToken'] = $list->nextPageToken;
+      $results['prevPageToken'] = $list->prevPageToken;
+      $results['totalResults'] = $list->pageInfo->totalResults;
+
+      foreach ($list->items as $item) {
+        $results['videos'][$item->id->videoId] = [
+          'id' => $item->id->videoId,
+          'title' => $item->snippet->title,
+          'cover' => self::cover($item->id->videoId),
+          'added' => false,
+        ];
+      }
+
+      $added = Video::whereIn('id', array_keys($results['videos']))
+        ->where('user_id', $user->id)
+        ->get()
+        ->pluck('id');
+
+      foreach ($added as $id) {
+        $results['videos'][$id]['added'] = true;
+      }
+
+      return $results;
+
+    }
+
+    /**
+     * Return a video cover URL from its id
+     *
+     */
+    public static function cover($id)
+    {
+      return 'https://i.ytimg.com/vi/'.$id.'/hqdefault.jpg';
+    }
+
+    /**
+     * Get YouTube videos
+     *
+     * @param Array $ids
+     * @return Object
+     */
+    public function getVideos($ids)
+    {
+
+      $videos = [];
+      $list = $this->youtube->videos->listVideos(
+        ['statistics,snippet'],
+        ['id' => implode(',', $ids), 'maxResults' => count($ids)]
+      );
+
+      foreach ($list->items as $item) {
+        $videos[$item->id] = [
+          'title' => $item->snippet->title,
+          'views' => $item->statistics->viewCount,
+        ];
+      }
+
+      return $videos;
+    }
+
+ }
